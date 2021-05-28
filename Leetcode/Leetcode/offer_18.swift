22ec4e82c947c1bc465f96a79cd234b200f621d0
langa <Itachi_invincible@163.com>
2021-05-28 23:31:10 +0800

    feat: 思路

--- Leetcode/Leetcode/offer_18.swift
+++ Leetcode/Leetcode/offer_18.swift
@@ -6,7 +6,28 @@
 //  Copyright © 2021 ZA. All rights reserved.
 //
 
-import Foundation
+/*
+ 本题删除值为 val 的节点分需为两步：定位节点、修改引用。
+
+ 定位节点： 遍历链表，直到 head.val == val 时跳出，即可定位目标节点。
+ 修改引用： 设节点 cur 的前驱节点为 pre ，后继节点为 cur.next ；则执行 pre.next = cur.next ，即可实现删除 cur 节点。
+
+
+ 算法流程：
+ 特例处理： 当应删除头节点 head 时，直接返回 head.next 即可。
+ 初始化： pre = head , cur = head.next 。
+ 定位节点： 当 cur 为空 或 cur 节点值等于 val 时跳出。
+ 保存当前节点索引，即 pre = cur 。
+ 遍历下一节点，即 cur = cur.next 。
+ 删除节点： 若 cur 指向某节点，则执行 pre.next = cur.next ；若 cur 指向 nullnull ，代表链表中不包含值为 val 的节点。
+ 返回值： 返回链表头部节点 head 即可。
+
+ 作者：jyd
+ 链接：https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof/solution/mian-shi-ti-18-shan-chu-lian-biao-de-jie-dian-sh-2/
+ 来源：力扣（LeetCode）
+ 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+
 /// 18. 删除链表的节点
 /// https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof/
 
--- Leetcode/Leetcode/offer_42.swift
+++ Leetcode/Leetcode/offer_42.swift
@@ -5,6 +5,43 @@
 //  Created by zza on 2021/5/28.
 //
 
+/*
+ 解题思路：
+ 常见解法    时间复杂度    空间复杂度
+ 暴力搜索    O(N^2)      O(1)
+ 分治思想    O(NlogN)    O(logN)
+ 动态规划    O(N)        O(1)
+ 动态规划是本题的最优解法，以下按照标准流程解题。
+ 动态规划解析：
+ 状态定义： 设动态规划列表 dpdp ，dp[i]dp[i] 代表以元素 nums[i]nums[i] 为结尾的连续子数组最大和。
+
+ 为何定义最大和 dp[i]dp[i] 中必须包含元素 nums[i]nums[i] ：保证 dp[i]dp[i] 递推到 dp[i+1]dp[i+1] 的正确性；如果不包含 nums[i]nums[i] ，递推时则不满足题目的 连续子数组 要求。
+ 转移方程： 若 dp[i-1] \leq 0dp[i−1]≤0 ，说明 dp[i - 1]dp[i−1] 对 dp[i]dp[i] 产生负贡献，即 dp[i-1] + nums[i]dp[i−1]+nums[i] 还不如 nums[i]nums[i] 本身大。
+
+ 当 dp[i - 1] > 0dp[i−1]>0 时：执行 dp[i] = dp[i-1] + nums[i]dp[i]=dp[i−1]+nums[i] ；
+ 当 dp[i - 1] \leq 0dp[i−1]≤0 时：执行 dp[i] = nums[i]dp[i]=nums[i] ；
+ 初始状态： dp[0] = nums[0]dp[0]=nums[0]，即以 nums[0]nums[0] 结尾的连续子数组最大和为 nums[0]nums[0] 。
+
+ 返回值： 返回 dpdp 列表中的最大值，代表全局最大值。
+
+ 空间复杂度降低：
+ 由于 dp[i]dp[i] 只与 dp[i-1]dp[i−1] 和 nums[i]nums[i] 有关系，因此可以将原数组 numsnums 用作 dpdp 列表，即直接在 numsnums 上修改即可。
+ 由于省去 dpdp 列表使用的额外空间，因此空间复杂度从 O(N)O(N) 降至 O(1)O(1) 。
+ 复杂度分析：
+
+ 时间复杂度 O(N)O(N) ： 线性遍历数组 numsnums 即可获得结果，使用 O(N)O(N) 时间。
+ 空间复杂度 O(1)O(1) ： 使用常数大小的额外空间。
+
+ 作者：jyd
+ 链接：https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/solution/mian-shi-ti-42-lian-xu-zi-shu-zu-de-zui-da-he-do-2/
+ 来源：力扣（LeetCode）
+ 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+
+/// 剑指 Offer 42. 连续子数组的最大和
+/// https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/
+
+
 private class Solution42 {
     func maxSubArray(_ nums: [Int]) -> Int {
         ///  异常处理
