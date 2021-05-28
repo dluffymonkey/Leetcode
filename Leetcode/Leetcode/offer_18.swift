fcd8a33f18fa627ef231d27dc5fb4c2602aa6a36
langa <langAB321@gmail.com>
2021-05-28 16:57:45 +0800

    feat: 剑指 Offer 18. 删除链表的节点

---
+++ Leetcode/Leetcode/offer_18.swift
@@ -0,0 +1,68 @@
+//
+//  offer_18.swift
+//  TestMac
+//
+//  Created by iLeo on 2021/5/28.
+//  Copyright © 2021 ZA. All rights reserved.
+//
+
+import Foundation
+/// 18. 删除链表的节点
+/// https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof/
+
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int, next: ListNode? = nil) {
+        self.val = val
+        self.next = next
+    }
+}
+extension ListNode: CustomStringConvertible {
+    public var description: String {
+        guard let next = next else {
+            print("\(val)")
+            return "\(val)"
+        }
+        let des = "\(val) -> " + String(describing: next)
+        return des
+    }
+}
+
+
+class Solution {
+    func deleteNode(_ head: ListNode?, _ val: Int) -> ListNode? {
+        if head == nil {
+            return nil
+        }
+        
+        if head!.val == val {
+            return head?.next
+        }
+        
+        var temp = head
+        while temp?.next != nil {
+            if temp?.next?.val == val {
+                temp?.next = temp?.next?.next
+                return head
+            } else {
+                temp = temp?.next
+            }
+        }
+        return temp
+    }
+}
+
+let n1 = ListNode(4)
+let n2 = ListNode(1)
+let n3 = ListNode(5)
+let n4 = ListNode(9)
+
+n1.next = n2
+n2.next = n3
+n3.next = n4
+
+let s = Solution()
+print(s.deleteNode(n1, 9))
+
