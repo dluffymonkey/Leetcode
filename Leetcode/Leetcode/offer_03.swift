76a9c91d9df0d83e41830e20299e831567eb1152
iLeo <Itachi_invincible@163.com>
2021-05-29 09:44:26 +0800

    剑指 Offer 03. 数组中重复的数字

---
+++ Leetcode/Leetcode/offer_03.swift
@@ -0,0 +1,71 @@
+//
+//  offer_03.swift
+//  Leetcode
+//
+//  Created by zza on 2021/5/29.
+//
+
+
+/*
+ 方法一：哈希表 / Set
+ 利用数据结构特点，容易想到使用哈希表（Set）记录数组的各个数字，当查找到重复数字则直接返回。
+
+ 算法流程：
+ 初始化： 新建 HashSet ，记为 dicdic ；
+ 遍历数组 nums 中的每个数字 num ：
+ 当 num 在 dic 中，说明重复，直接返回 num ；
+ 将 num 添加至 dic 中；
+ 返回 -1 。本题中一定有重复数字，因此这里返回多少都可以。
+ 复杂度分析：
+ 时间复杂度 O(N) ： 遍历数组使用 O(N) ，HashSet 添加与查找元素皆为 O(1) 。
+ 空间复杂度 O(N) ： HashSet 占用 O(N) 大小的额外空间。
+
+ 
+ 
+ 方法二：原地交换
+ 题目说明尚未被充分使用，即 在一个长度为 n 的数组 nums 里的所有数字都在 0 ~ n-1 的范围内 。 此说明含义：数组元素的 索引 和 值 是 一对多 的关系。
+ 因此，可遍历数组并通过交换操作，使元素的 索引 与 值 一一对应（即 nums[i] = i）。因而，就能通过索引映射对应的值，起到与字典等价的作用。
+
+ 遍历中，第一次遇到数字 x 时，将其交换至索引 x 处；而当第二次遇到数字 x 时，一定有 nums[x] = x ，此时即可得到一组重复数字。
+
+ 算法流程：
+ 遍历数组 nums ，设索引初始值为 i = 0 :
+
+ 若 nums[i] = i ： 说明此数字已在对应索引位置，无需交换，因此跳过；
+ 若 nums[nums[i]]=nums[i] ： 代表索引 nums[i] 处和索引 i 处的元素值都为 nums[i] ，即找到一组重复值，返回此值 nums[i] ；
+ 否则：交换索引为 i 和 nums[i] 的元素值，将此数字交换至对应索引位置。
+ 若遍历完毕尚未返回，则返回 −1 。
+
+ 复杂度分析：
+ 时间复杂度 O(N) ： 遍历数组使用 O(N) ，每轮遍历的判断和交换操作使用 O(1) 。
+ 空间复杂度 O(1) ： 使用常数复杂度的额外空间。
+
+ 作者：jyd
+ 链接：https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/solution/mian-shi-ti-03-shu-zu-zhong-zhong-fu-de-shu-zi-yua/
+ 来源：力扣（LeetCode）
+ 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+
+/// 剑指 Offer 03. 数组中重复的数字
+/// https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
+
+class Solution03 {
+    func findRepeatNumber(_ nums: [Int]) -> Int {
+        var set = Set<Int>()
+        for item in nums {
+            if set.contains(item) {
+                return item
+            } else {
+                set.insert(item)
+            }
+        }
+        return -1
+    }
+}
+
+
+func test03() {
+    let s03 = Solution03()
+    let res = s03.findRepeatNumber([2, 3, 1, 0, 2, 5, 3])
+    print("res --- \(res)")
+}
--- Leetcode/Leetcode/offer_42.swift
+++ Leetcode/Leetcode/offer_42.swift
@@ -13,24 +13,22 @@
  动态规划    O(N)        O(1)
  动态规划是本题的最优解法，以下按照标准流程解题。
  动态规划解析：
- 状态定义： 设动态规划列表 dpdp ，dp[i]dp[i] 代表以元素 nums[i]nums[i] 为结尾的连续子数组最大和。
+ 状态定义： 设动态规划列表 dp ，dp[i] 代表以元素 nums[i] 为结尾的连续子数组最大和。
 
- 为何定义最大和 dp[i]dp[i] 中必须包含元素 nums[i]nums[i] ：保证 dp[i]dp[i] 递推到 dp[i+1]dp[i+1] 的正确性；如果不包含 nums[i]nums[i] ，递推时则不满足题目的 连续子数组 要求。
- 转移方程： 若 dp[i-1] \leq 0dp[i−1]≤0 ，说明 dp[i - 1]dp[i−1] 对 dp[i]dp[i] 产生负贡献，即 dp[i-1] + nums[i]dp[i−1]+nums[i] 还不如 nums[i]nums[i] 本身大。
-
- 当 dp[i - 1] > 0dp[i−1]>0 时：执行 dp[i] = dp[i-1] + nums[i]dp[i]=dp[i−1]+nums[i] ；
- 当 dp[i - 1] \leq 0dp[i−1]≤0 时：执行 dp[i] = nums[i]dp[i]=nums[i] ；
- 初始状态： dp[0] = nums[0]dp[0]=nums[0]，即以 nums[0]nums[0] 结尾的连续子数组最大和为 nums[0]nums[0] 。
-
+ 为何定义最大和 dp[i] 中必须包含元素 nums[i] ：保证 dp[i] 递推到 dp[i+1] 的正确性；如果不包含 nums[i] ，递推时则不满足题目的连续子数组要求。
+ 转移方程： 若 dp[i−1] ≤ 0 ，说明 dp[i - 1] 对 dp[i] 产生负贡献，即 dp[i-1] + nums[i] 还不如 nums[i] 本身大。
+ 当 dp[i - 1] > 0 时：执行 dp[i] = dp[i-1] + nums[i] ；
+ 当 dp[i − 1] ≤ 0 时：执行 dp[i] = nums[i] ；
+ 初始状态： dp[0] = nums[0]，即以 nums[0] 结尾的连续子数组最大和为 nums[0] 。
  返回值： 返回 dpdp 列表中的最大值，代表全局最大值。
 
  空间复杂度降低：
- 由于 dp[i]dp[i] 只与 dp[i-1]dp[i−1] 和 nums[i]nums[i] 有关系，因此可以将原数组 numsnums 用作 dpdp 列表，即直接在 numsnums 上修改即可。
- 由于省去 dpdp 列表使用的额外空间，因此空间复杂度从 O(N)O(N) 降至 O(1)O(1) 。
+ 由于 dp[i] 只与 dp[i-1] 和 nums[i] 有关系，因此可以将原数组 nums 用作 dp 列表，即直接在 nums 上修改即可。
+ 由于省去 dp 列表使用的额外空间，因此空间复杂度从 O(N) 降至 O(1) 。
  复杂度分析：
 
- 时间复杂度 O(N)O(N) ： 线性遍历数组 numsnums 即可获得结果，使用 O(N)O(N) 时间。
- 空间复杂度 O(1)O(1) ： 使用常数大小的额外空间。
+ 时间复杂度 O(N) ： 线性遍历数组 nums 即可获得结果，使用 O(N) 时间。
+ 空间复杂度 O(1) ： 使用常数大小的额外空间。
 
  作者：jyd
  链接：https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/solution/mian-shi-ti-42-lian-xu-zi-shu-zu-de-zui-da-he-do-2/
@@ -38,9 +36,10 @@
  著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
  */
 
+/// **注意**：动态规划主要是看以当前数组的某一个元素为结尾的连续子数组的最大和，必须带上某个元素为最后一个值，然后从第一个元素开始计算，同时判断元素的值是否大于0，把得到的最大值存放到数组Q中，最后取Q的最大值即可
+
 /// 剑指 Offer 42. 连续子数组的最大和
 /// https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/
-
 
 private class Solution42 {
     func maxSubArray(_ nums: [Int]) -> Int {
